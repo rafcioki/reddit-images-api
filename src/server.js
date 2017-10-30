e1440ab9fd5456a74da21111236bd43ebbc53704
Rafal Ostrowski <oester200729@gmail.com>
2017-10-31 00:26:37 +0100

    Removed some duplicated code.

--- src/server.js
+++ src/server.js
@@ -1,39 +1,24 @@
 import rp from 'request-promise';
 import * as constants from './consts';
+import FilterNonImageUrls from './parsing';
 
 const GetTopImagesFromSubreddit = (subreddit,
     sort = constants.SORT_TOP) => {
   rp(`https://www.reddit.com/r/${subreddit}.json?sort=${sort}`)
     .then(response => {
-      const allowedExtensions = constants
-        .AllowedImageExtensions
-        .split(';')
-        .map(extension => {
-          return `.${extension}`;
-      });
-
-      console.log(allowedExtensions);
-
       const parsedRootJson = JSON.parse(response);
 
-      const validImageUrls = parsedRootJson.data.children.map(child => {
+      const imageUrls = parsedRootJson.data.children.map(child => {
         return child.data.url;
-      }).filter(url => {
-        for (let i = 0; i < allowedExtensions.length; ++i) {
-          if (url.endsWith(allowedExtensions[i])) {
-            return true;
-          }
-        }
-
-        return false;
       });
-
-      console.log(validImageUrls);
+      
+      const validImageUrls = FilterNonImageUrls(constants.AllowedImageExtensions, imageUrls);
     })
     .catch(error => {
+      console.log(error);
     });
 };
 
-GetTopImagesFromSubreddit('architectureporn');
+// GetTopImagesFromSubreddit('architectureporn');
 
 export default GetTopImagesFromSubreddit;
